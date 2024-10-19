1e6f83fa594aeaa352f86939d42fdf4af50e7944
Lucas Oliveira <lucasoliveiralog@hotmail.com>
2024-10-19 15:10:31 -0300

    Add context, hook and components

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ContextComponent from './context/context'
 import './index.css'
 import Home from './pages/home'
-import ContextComponent from './context/context'
 
 createRoot(document.getElementById('root')!).render(
   <ContextComponent>
